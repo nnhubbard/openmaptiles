d44c45297b0907bd7146563652e471abef25472c
Nicholas Hubbard <nic@zedsaid.com>
2024-02-06 05:09:51 -0800

    Added new cachly_pro layer. This will be a new layer for any additions that don't fit in other layers.

---
+++ layers/cachly_pro/cachly_pro.sql
@@ -0,0 +1,11 @@
+-- Function for layer query
+CREATE OR REPLACE FUNCTION layer_cachly_pro(bbox geometry, zoom_level int)
+RETURNS TABLE(geometry geometry, 
+	historic text) AS $$
+    	
+    SELECT geometry, 
+    historic
+    FROM osm_cachly_pro_linestring
+    WHERE zoom_level >= 10 AND geometry && bbox 
+    
+$$ LANGUAGE SQL IMMUTABLE;
