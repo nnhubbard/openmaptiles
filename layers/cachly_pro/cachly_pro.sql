bd65465f1c3d572e17964714021b2bd9d01c54fd
Nicholas Hubbard <nic@zedsaid.com>
2024-02-11 16:45:12 -0800

    Cachly Pro Layer: Added building in order to exclude historic that are already in the building layer

--- layers/cachly_pro/cachly_pro.sql
+++ layers/cachly_pro/cachly_pro.sql
@@ -1,11 +1,21 @@
 -- Function for layer query
 CREATE OR REPLACE FUNCTION layer_cachly_pro(bbox geometry, zoom_level int)
 RETURNS TABLE(geometry geometry, 
-	historic text) AS $$
+	historic text,
+	building text) AS $$
     	
     SELECT geometry, 
-    historic
+    historic,
+    nullif(building, '') AS building
     FROM osm_cachly_pro_linestring
-    WHERE zoom_level >= 14 AND geometry && bbox 
+    WHERE zoom_level >= 14 AND geometry && bbox
+    
+    UNION ALL
+    
+    SELECT geometry, 
+    historic,
+    nullif(building, '') AS building
+    FROM osm_cachly_pro_polygon
+   	WHERE building = '' AND zoom_level >= 14 AND geometry && bbox
     
 $$ LANGUAGE SQL IMMUTABLE;
