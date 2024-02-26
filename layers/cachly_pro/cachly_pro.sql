b7c1c463722d87866408a70fdd338c9061b2a18f
Nicholas Hubbard <nic@zedsaid.com>
2024-02-26 07:00:49 -0800

    Cachly Pro: Added city_wall

--- layers/cachly_pro/cachly_pro.sql
+++ layers/cachly_pro/cachly_pro.sql
@@ -2,19 +2,22 @@
 CREATE OR REPLACE FUNCTION layer_cachly_pro(bbox geometry, zoom_level int)
 RETURNS TABLE(geometry geometry, 
 	historic text,
+	barrier text,
 	building text) AS $$
     	
     SELECT geometry, 
-    historic,
-    nullif(building, '') AS building
+    NULLIF(historic, '') AS historic,
+    NULLIF(barrier, '') AS barrier,
+    NULLIF(building, '') AS building
     FROM osm_cachly_pro_linestring
     WHERE zoom_level >= 14 AND geometry && bbox
     
     UNION ALL
     
     SELECT geometry, 
-    historic,
-    nullif(building, '') AS building
+    NULLIF(historic, '') AS historic,
+    NULLIF(barrier, '') AS barrier,
+    NULLIF(building, '') AS building
     FROM osm_cachly_pro_polygon
    	WHERE building = '' AND zoom_level >= 14 AND geometry && bbox
     
